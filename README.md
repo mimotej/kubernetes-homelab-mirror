e8014f902c7e6f9f97b09440c91a0128cce9a93a
Michal Drla <michal.drla@outlook.com>
2026-02-17 20:15:21 +0100

    feat: Initial commit
    Signed-off-by: Michal Drla <michal.drla@outlook.com>

---
+++ README.md
@@ -0,0 +1,3 @@
+# TODO
+
+This repository is only mirror of my kubernetes homelab configuration, therefore issues and prs are disabled and I will update this repository as time goes on.
---
+++ dockerfiles/bitwarden-cli/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+bw config server ${BW_HOST}
+
+if [ -n "$BW_CLIENTID" ] && [ -n "$BW_CLIENTSECRET" ]; then
+    echo "Using apikey to log in"
+    export BW_CLIENTID=$(echo -n ${BW_CLIENTID})
+    export BW_CLIENTSECRET=$(echo -n ${BW_CLIENTSECRET})
+    export BW_PASSWORD=$(echo -n ${BW_PASSWORD})
+    bw login --apikey --raw
+    export BW_SESSION=$(bw unlock --passwordenv BW_PASSWORD --raw)
+else
+    echo "Using username and password to log in"
+    export BW_SESSION=$(bw login ${BW_USER} --passwordenv BW_PASSWORD --raw)
+fi
+
+bw unlock --check
+
+echo 'Running `bw server` on port 8087'
+bw serve --hostname 0.0.0.0 #--disable-origin-protection
